1abf06fb2cf26d52c8061e325ceede9973805839
Rowan-Paul <rpflynn80@gmail.com>
2021-04-11 18:07:58 +0200

    Add /api/v1/cards route

--- routes/api/v1/cards.js
+++ routes/api/v1/cards.js
@@ -6,7 +6,13 @@ const router = express.Router()
 const Cards = require('../../../models/Cards')
 
 router.get('/', (req, res) => {
-  res.status(200).send('hello world')
+  Cards.find({}, '-__v', (err, cards) => {
+    if (err) {
+      return res.status(500)
+    }
+
+    return res.status(200).send(cards)
+  })
 })
 
 module.exports = router
