7da58d65d6d5df16a1ab3dbf3617cd97d92a3265
Rowan-Paul <rpflynn80@gmail.com>
2021-04-13 16:11:42 +0200

    Add decks and deck route

--- routes/api/v1/cards.js
+++ routes/api/v1/cards.js
@@ -36,4 +36,36 @@ router.get('/:card', (req, res) => {
   )
 })
 
+router.get('/decks', (req, res) => {
+  const decks = {
+    decks: [
+      'Neutral',
+      'Monsters',
+      'Nilfgaard',
+      'Northern Realms',
+      "Scoia'tael",
+      'Skellige',
+    ],
+  }
+  return res.status(200).send(decks)
+})
+
+router.get('/decks/:deck', (req, res) => {
+  const { deck } = req.params
+
+  Cards.find({ deck: deck }, { __v: 0 }, (err, cards) => {
+    if (err) {
+      return res.status(500)
+    }
+
+    const deckCards = {
+      deck: deck,
+      amount: cards.length,
+      cards: cards,
+    }
+
+    return res.status(200).send(deckCards)
+  })
+})
+
 module.exports = router
