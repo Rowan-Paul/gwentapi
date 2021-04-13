5ba61e846ada8d1b48f4e0c530eb184cc62bf211
Rowan-Paul <rpflynn80@gmail.com>
2021-04-13 16:11:12 +0200

    Add single card route

--- routes/api/v1/cards.js
+++ routes/api/v1/cards.js
@@ -15,4 +15,25 @@ router.get('/', (req, res) => {
   })
 })
 
+router.get('/:card', (req, res) => {
+  const { card } = req.params
+
+  var ObjectId = require('mongoose').Types.ObjectId
+  var objId = new ObjectId(card.length < 12 ? '123456789012' : card)
+
+  Cards.findOne(
+    {
+      $or: [{ _id: objId }, { name: card }],
+    },
+    { __v: 0 },
+    (err, card) => {
+      if (err) {
+        return res.status(500)
+      }
+
+      return res.status(200).send(card)
+    }
+  )
+})
+
 module.exports = router
