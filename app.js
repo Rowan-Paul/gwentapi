d64bb24ae303bc2c690344e3b08b776390526499
Rowan-Paul <rpflynn80@gmail.com>
2021-05-01 16:24:19 +0200

    Add serve frontend

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || '3000'
@@ -16,9 +17,15 @@ const cardsRouter = require('./routes/api/v1/cards')
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(cors())
+app.use(express.static('.'))
 
 // ROUTES MIDDLEWARE
 app.use('/api/v1/cards', cardsRouter)
+
+// SERVE SITE
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../GWENTcards/build', 'index.html'))
+})
 
 // CREATE SERVER
 const server = app.listen(port, () => {
