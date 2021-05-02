3c889031ec683276fd2d6846c5f435315b345df4
Rowan-Paul <rpflynn80@gmail.com>
2021-05-02 16:21:30 +0200

    Add signin route (without token)

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -55,4 +55,72 @@ router.post('/signup', (req, res) => {
   )
 })
 
+// Signin to account
+router.post('/signin', (req, res) => {
+  const { body } = req
+  const { password } = body
+  let { email } = body
+
+  if (!email) {
+    return res.status(401).send('Error: Email cannot be blank.')
+  }
+  if (!password) {
+    return res.status(401).send('Error: Password cannot be blank.')
+  }
+
+  email = email.toLowerCase()
+  email = email.trim()
+
+  User.find(
+    {
+      email: email,
+    },
+    (err, users) => {
+      if (err) {
+        return res.sendStatus(500)
+      }
+
+      if (users.length != 1) {
+        return res.status(401).send('Error: Invalid username')
+      }
+
+      const user = users[0]
+
+      if (!user.validPassword(password)) {
+        return res.status(401).send('Error: Invalid password')
+      }
+
+      // Otherwise correct user
+      return res.sendStatus(200)
+
+      //   const userSession = new UserSession()
+      //   userSession.userId = user._id
+
+      //   userSession.save((err, doc) => {
+      //     if (err) {
+      //       console.log(err)
+      //       return res.sendStatus(500)
+      //     }
+
+      //     let filteredUser = {
+      //       email: user.email,
+      //       firstName: user.firstName,
+      //       lastName: user.lastName,
+      //       isAdmin: user.isAdmin,
+      //     }
+
+      //     return res.status(201).send({
+      //       message: 'Signed in',
+      //       token: encryptor.encrypt({
+      //         random: doc.random,
+      //         id: doc._id,
+      //         timestamp: doc.timestamp,
+      //       }),
+      //       user: filteredUser,
+      //     })
+      //   })
+    }
+  )
+})
+
 module.exports = router
