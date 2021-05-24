e8d5338a0aba1df5648ef36ca652ffb5643e1985
Rowan-Paul <contact@rowanpaulflynn.com>
2021-05-24 13:56:46 +0200

    Update id to locations & rename cards collected

--- models/Locations.js
+++ models/Locations.js
@@ -1,26 +1,23 @@
 const mongoose = require('mongoose')
 
-const LocationsSchema = new mongoose.Schema(
-  {
-    type: {
-      type: String,
-      required: true,
-      enum: ['quest', 'buy', 'base deck', 'random', 'win', 'summoned'],
-    },
-    territory: {
-      type: String,
-      required: false,
-    },
-    location: {
-      type: String,
-      required: false,
-    },
-    character: {
-      type: String,
-      required: false,
-    },
+const LocationsSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: ['quest', 'buy', 'base deck', 'random', 'win', 'summoned'],
   },
-  { _id: false }
-)
+  territory: {
+    type: String,
+    required: false,
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+  character: {
+    type: String,
+    required: false,
+  },
+})
 
 module.exports = LocationsSchema
